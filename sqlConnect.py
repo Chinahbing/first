1406a4f8c07e08a48f9016ca45ccc455d439728b
hubing <2210866755@qq.com>
2021-06-09 13:17:40 +0800

    添加注释

--- sqlConnect.py
+++ sqlConnect.py
@@ -2,14 +2,15 @@ from typing import Counter
 import pymysql
 import json
 
+# 从config.txt 文件中获取数据库信息
 f = open("config.txt", "r", encoding="utf-8")
 data = json.loads(f.read())
-
 dbhost = data['dbhost']
 dbuser = data['dbuser']
 dbpass = data['dbpass']
 dbname = data['dbname']
 
+# 连接数据库
 db = pymysql.connect(host=dbhost,user= dbuser,password= dbpass, database=dbname)
 # 使用 cursor() 方法创建一个游标对象 cursor
 cursor = db.cursor()
